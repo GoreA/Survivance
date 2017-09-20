83d39d781f0411e267c3f9f03e6d412e9e560702
Alina Gore <agore@inther.local>
2017-09-20 14:04:16 +0300

    Added theta calculus function with regularisation and sigmoid

--- src/main/java/alisgroup/titanicmanipulator/Gradient.java
+++ src/main/java/alisgroup/titanicmanipulator/Gradient.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 public class Gradient {
 
   public static float[] calculateTheta(Matrix matrix, float alpha, int iterations) {
-    //initialise theta. All values should be equal to 0 as starters. The length of Theta should be exual to the number
+    //initialise theta. All values should be equal to 0 as starters. The length of Theta should be equal to the number
     //of parameters from one matrixX row
     float[] theta = new float[matrix.getMatrixX()[1].length];
     float[][] theta_hist = new float[iterations][matrix.getMatrixX()[1].length];
@@ -41,7 +41,7 @@ public class Gradient {
         }
       }
 
-      // TODO need to update J after each iteration.
+      // update J after each iteration.
       double J_part = 0;
       for (int t = 0; t < m; t++) {
         J_part = J_part + Math.pow((cost[t] - matrix.getMatrixY()[t]), 2);
@@ -62,6 +62,63 @@ public class Gradient {
     return theta_hist[best_iterration];
   }
 
+  public static double[] calculateThetaWithRegularization(Matrix matrix, float alpha, int iterations) {
+    //initialise theta. All values should be equal to 0 as starters. The length of Theta should be equal to the number
+    //of parameters from one matrixX row
+    double[] theta = new double[matrix.getMatrixX()[1].length];
+    double[][] theta_hist = new double[iterations][matrix.getMatrixX()[1].length];
+    int m = matrix.getMatrixY().length;
+//        initialise cost
+    double[] cost = new double[m];
+//        initialise J
+    double[] J_history = new double[iterations];
+    for (int i = 0; i < J_history.length; i++) {
+      J_history[i] = 1000;
+    }
+
+//        here the iterations begin
+    for (int i = 0; i < iterations; i++) {
+
+//          update theta
+// TODO need to add regularisation.
+      for (int l = 0; l < theta.length; l++) {
+        double div = 0;
+        for (int ll = 0; ll < m; ll++) {
+          div = div + (cost[ll] - matrix.getMatrixY()[ll]) * matrix.getMatrixX()[ll][l];
+        }
+        theta[l] = theta[l] - (alpha / m) * div;
+      }
+//        calculate the cost with obtained theta
+      for (int j = 0; j < m; j++) {
+        float[] vectorX = matrix.getMatrixX()[j];
+        cost[j] = 0;
+        for (int k = 0; k < theta.length; k++) {
+          cost[j] = cost[j] + theta[k] * vectorX[k];
+        }
+        //sigmoid function
+        cost[j] = 1/(1 + Math.exp(-cost[j]));
+      }
+
+      // TODO need to add regularisation.
+      double J_part = 0;
+      for (int t = 0; t < m; t++) {
+        J_part = J_part + Math.pow((cost[t] - matrix.getMatrixY()[t]), 2);
+      }
+      // if we put 1 / (2 * m) instead of just /(2*m) JVM returns us 0. In this case the J will always be 0
+      J_history[i] = J_part / (2 * m);
+      theta_hist[i] = theta;
+    }
+    double J_min = J_history[0];
+    int best_iterration = 0;
+    for (int i = 1; i < J_history.length; i++) {
+      if (J_history[i] < J_min) {
+        J_min = J_history[i];
+        best_iterration = i;
+      }
+    }
+    System.out.println(best_iterration);
+    return theta_hist[best_iterration];
+  }
   public static float calculateCost(float[] theta, Person p, Method... methods) {
     float cost = theta[0];
     for (int i = 1; i < theta.length; i++) {
